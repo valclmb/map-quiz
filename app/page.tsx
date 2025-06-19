b61f28c4b613cdd9e3bd8fcde1be3b5d4fd61bc8
Tenma <tenma@osxt.com>
2025-06-19 13:32:36 +0200

    init auth

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { getCountries } from "./lib/data";
 
 export default async function Home() {
   const countries = await getCountries();
+
   return (
     <main className="flex max-h-screen overflow-hidden flex-col items-center justify-between p-10">
       <Game countries={countries ?? []} />
--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { authClient } from "@/lib/auth-client";
+import { Button } from "../ui/button";
 import { Checkbox } from "../ui/checkbox";
 import { Label } from "../ui/label";
 
@@ -27,8 +29,17 @@ export const Filter = ({ setFilter }: FilterProps) => {
     });
   };
 
+  const signUp = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+    });
+  };
+
+  const session = authClient.useSession();
+  console.log(session);
   return (
     <nav className="flex gap-3 z-50 p-2 absolute right-1/2 top-5 translate-x-1/2 border border-background bg-background/50 rounded-sm backdrop-blur-md ">
+      <Button onClick={signUp}>Sign up</Button>
       {continents.map((continent) => (
         <div key={continent} className="flex items-center gap-1">
           <Checkbox
